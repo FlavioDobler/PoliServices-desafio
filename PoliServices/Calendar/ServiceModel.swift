8a3fe78ec35cacce717d4b8073ef8babd7450a2f
Flavio Dobler <flaviodobler@Flavios-MacBook-Air.local>
2023-07-24 10:58:20 -0300

    ViewModel and Extensions

---
+++ PoliServices/Calendar/ServiceModel.swift
@@ -0,0 +1,8 @@
+//
+//  ServiceModel.swift
+//  PoliServices
+//
+//  Created by Flavio Dobler on 24/07/23.
+//
+
+import Foundation
---
+++ PoliServices/Extensions/DateExtensions.swift
@@ -0,0 +1,8 @@
+//
+//  DateExtensions.swift
+//  PoliServices
+//
+//  Created by Flavio Dobler on 18/07/23.
+//
+
+import Foundation
